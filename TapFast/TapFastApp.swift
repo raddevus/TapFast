62e2cdadbb59b9240a354b50e11dd4c75bc76c60
raddevus <1radeutsch1@gmail.com>
2025-07-17 14:33:40 -0400

    Initial Commit

---
+++ TapFast/TapFastApp.swift
@@ -0,0 +1,17 @@
+//
+//  TapFastApp.swift
+//  TapFast
+//
+//  Created by roger deutsch on 7/17/25.
+//
+
+import SwiftUI
+
+@main
+struct TapFastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TapFastTests/TapFastTests.swift
@@ -0,0 +1,17 @@
+//
+//  TapFastTests.swift
+//  TapFastTests
+//
+//  Created by roger deutsch on 7/17/25.
+//
+
+import Testing
+@testable import TapFast
+
+struct TapFastTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
